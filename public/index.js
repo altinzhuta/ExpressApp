1575fe2b564cc24bce40cdb2101bba18b959e0e9
altinzhuta <altin.zuta@hotmail.de>
2020-04-26 02:14:10 +0200

    vulkanausbruch

--- public/index.js
+++ public/index.js
@@ -1,50 +1,50 @@
-
 // id from url todo *****
-let id1 = '5e98d9b6a2fe443858997e7e';
-let id2 = '5e9926bd866a611a7080ee40';
-
+let id1 = "5ea4cd2d72ddd01f3413d4ed";
+let id2 = "5e9926bd866a611a7080ee40";
 
 $(document).ready(function () {
-    $('#btn').click(function () {
-        $.ajax({
-            beforeSend: function () {
-                $('#results').html('Rechne auf Server...');
-            },
-            type: 'GET', url: `/users/${id1}`, success: function (data) {
-                $('#results').text("user name with id- " + id1 + " : " + data.name);
-            }
-        });
-    });
-    $('#btn2').click(function () {
-        $.ajax({
-            beforeSend: function () {
-                $('#results2').html('Rechne auf Server...');
-            },
-            type: 'GET', url: `/users`, success: function (data) {
-                $('#results2').text("number of users : " + data.length);
-            }
-        });
-    });
-    $('#btn3').click(function () {
-        $.ajax({
-            beforeSend: function () {
-                $('#results3').html('Rechne auf Server...');
-            },
-            type: 'GET', url: `/events`, success: function (data) {
-                $('#results3').text("number of events : " + data.length);
-            }
-        });
-    });
-    $('#btn4').click(function () {
-        $.ajax({
-            beforeSend: function () {
-                $('#results4').html('Rechne auf Server...');
-            },
-            type: 'GET', url: `/events/${id2}`, success: function (data) {
-                $('#results4').text("eventname mit " + id2 + " : " + data.name);
-            }
-        });
-    });
+  $.ajax({
+    beforeSend: function () {
+      $("#p1").html("Rechne auf Server...");
+    },
+    type: "GET",
+    url: `/events/${id1}`,
+    success: function (data) {
+      $("#p1").text(data.price + " €");
+      $("#h3_1").html(data.name);
+    },
+  });
+
+  $.ajax({
+    beforeSend: function () {
+      $("#p2").html("Rechne auf Server...");
+    },
+    type: "GET",
+    url: `/users`,
+    success: function (data) {
+      $("#p2").text("number of users : " + data.length);
+    },
+  });
+
+  $.ajax({
+    beforeSend: function () {
+      $("#p3").html("Rechne auf Server...");
+    },
+    type: "GET",
+    url: `/events`,
+    success: function (data) {
+      $("#p3").text("number of events : " + data.length);
+    },
+  });
+
+  $.ajax({
+    beforeSend: function () {},
+    type: "GET",
+    url: `/events/${id2}`,
+    success: function (data) {
+      $("#h3_1").text("eventname mit " + id2 + " : " + data.name);
+    },
+  });
 });
 
 /* 
